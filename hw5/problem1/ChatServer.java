6d19f776aaef5ff378a34c570421348a86f74c90
Celine Seghbossian <celineblaine@icloud.com>
2023-04-30 13:22:10 -0700

    user and server

---
+++ hw5/problem1/ChatServer.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class ChatServer {
+    private Map<String, User> users;
+    private Map<User, List<User>> blockedUsers;
+
+    public ChatServer() {=
+        this.users = new HashMap<>();
+        this.blockedUsers = new HashMap<>();
+    }
+
+    public void registerUser(User user) {
+        this.users.put(user.getName(), user);
+        this.blockedUsers.put(user, new ArrayList<>());
+    }
+
+    public void unregisterUser(User user) {
+        this.users.remove(user.getName());
+        this.blockedUsers.remove(user);
+    }
+
+    public void sendMessage(Message message) {
+        for (User recipient : message.getRecipients()) {
+            if (!this.blockedUsers.get(recipient).contains(message.getSender())) {
+                recipient.receiveMessage(message);
+            }
+        }
+    }
+
+    public void blockUser(User user, User blockedUser) {
+        if (!this.blockedUsers.get(user).contains(blockedUser)) {
+            this.blockedUsers.get(user).add(blockedUser);
+        }
+    }
+
+    public void unblockUser(User user, User unblockedUser) {
+        this.blockedUsers.get(user).remove(unblockedUser);
+    }
+
+}
+
+
+
--- hw5/problem1/User.java
+++ hw5/problem1/User.java
@@ -4,12 +4,45 @@ import java.util.List;
 
 public class User {
     private String name;
+    private ChatServer chatServer;
     private ChatHistory chatHistory;
     private List<MessageMemento> messageHistory;
 
     public User(String name, ChatServer chatServer) {
         this.name = name;
+        this.chatServer = chatServer;
+        this.chatHistory = new ChatHistory();
         this.messageHistory = new ArrayList<>();
+        this.chatServer.registerUser(this);
+    }
+
+    public void sendMessage(User[] recipients, String content) {
+        Message message = new Message(this, recipients, content);
+        this.chatServer.sendMessage(message);
+        this.chatHistory.addMessage(message);
+        this.messageHistory.add(new MessageMemento(message.getContent(), message.getTimestamp()));
+    }
+
+    public void receiveMessage(Message message) {
+        if (!this.chatHistory.containsMessage(message)) {
+            this.chatHistory.addMessage(message);
+        }
+    }
+
+    public void undoLastMessage() {
+        if (!this.messageHistory.isEmpty()) {
+            MessageMemento lastMessage = this.messageHistory.get(this.messageHistory.size() - 1);
+            this.chatHistory.removeLastMessage();
+            this.messageHistory.remove(this.messageHistory.size() - 1);
+        }
+    }
+
+    public void blockUser(User user) {
+        this.chatServer.blockUser(this, user);
+    }
+
+    public void unblockUser(User user) {
+        this.chatServer.unblockUser(this, user);
     }
 
     public String getName() {
